836d932a33fc5207708123129c58b34b0b28a933
rifatjahan823 <rifatjahan823@gmail.com>
2023-10-03 12:18:22 +0600

    create reduxstore and react router

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import { Provider } from 'react-redux'
+import { router } from './router/router'
+import { store } from './redux/store'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
